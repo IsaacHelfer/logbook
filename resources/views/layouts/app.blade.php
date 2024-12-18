f1e36f4988380050f0daf06a8a3ddae758015c07
IsaacHelfer <ikelbereger30@gmail.com>
2024-12-18 15:31:31 -0500

    adding boostrap 5

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>{{ $title ?? 'Logbook' }}</title>
+
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
     <body>
         <header>
