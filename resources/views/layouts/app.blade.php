c1b9ee83bf10f867009dd9dbcffd69755e61e8aa
IsaacHelfer <ikelbereger30@gmail.com>
2024-12-18 16:42:07 -0500

    WIP adding add entry modal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,12 @@
 
     <title>{{ $title ?? 'Logbook' }}</title>
 
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+
+{{--    @vite(['resources/sass/app.scss', 'resources/js/app.js'])--}}
 </head>
     <body class="d-flex flex-column min-vh-100">
         <header>
