4284bce9945dfe659cf84fa8ccbed1577e574268
Isaac <76602451+IsaacHelfer@users.noreply.github.com>
2024-12-23 14:41:47 -0500

    restyle of logbook page

--- resources/views/entries/index.blade.php
+++ resources/views/entries/index.blade.php
@@ -3,8 +3,8 @@
 @props(['title'=>'Logbook'])
 
 @section('content')
-    <div class="card p-4">
-        <div class="card-header row align-items-center">
+    <div class="col-md-12 mt-2">
+        <div class="row align-items-center">
             <div class="col">
                 <h1>{{ $title }}</h1>
             </div>
@@ -14,81 +14,80 @@
                 </a>
             </div>
         </div>
+    </div>
 
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-hover table-striped mt-3">
-                    <thead>
+    <div class="table-responsive">
+        <table class="table table-hover table-striped mt-3">
+            <thead>
+            <tr>
+                <th scope="col">Date</th>
+                <th scope="col">Aircraft</th>
+                <th scope="col">Category</th>
+                <th scope="col">Time</th>
+                <th scope="col">Type</th>
+                <th scope="col">Time</th>
+                <th scope="col">Day</th>
+                <th scope="col">Night</th>
+                <th scope="col">XC</th>
+                <th scope="col">Instrument</th>
+                <th scope="col">Sim Instrument</th>
+                <th scope="col">Instrument Apps</th>
+                <th scope="col">Day Landings</th>
+                <th scope="col">Night Landings</th>
+                <th scope="col">Total Duration</th>
+                <th scope="col">Remarks</th>
+                <th scope="col">Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            @if(!empty($entries))
+                @foreach($entries as $entry)
                     <tr>
-                        <th scope="col">Date</th>
-                        <th scope="col">Aircraft</th>
-                        <th scope="col">Category</th>
-                        <th scope="col">Time</th>
-                        <th scope="col">Type</th>
-                        <th scope="col">Time</th>
-                        <th scope="col">Day</th>
-                        <th scope="col">Night</th>
-                        <th scope="col">XC</th>
-                        <th scope="col">Instrument</th>
-                        <th scope="col">Sim Instrument</th>
-                        <th scope="col">Instrument Apps</th>
-                        <th scope="col">Day Landings</th>
-                        <th scope="col">Night Landings</th>
-                        <th scope="col">Total Duration</th>
-                        <th scope="col">Remarks</th>
-                        <th scope="col">Actions</th>
+                        <td class="text-truncate">{{$entry->date}}</td>
+                        <td>{{$entry->aircraft->identifier ?? ''}}</td>
+                        <td>{{$entry->category->category ?? ''}}</td>
+                        <td>{{$entry->category_time ?? ''}}</td>
+                        <td>{{$entry->type->type ?? ''}}</td>
+                        <td>{{$entry->type_time ?? ''}}</td>
+                        <td>{{$entry->day_time ?? ''}}</td>
+                        <td>{{$entry->night_time ?? ''}}</td>
+                        <td>{{$entry->xc_time ?? ''}}</td>
+                        <td>{{$entry->actual_instrument ?? ''}}</td>
+                        <td>{{$entry->sim_instrument ?? ''}}</td>
+                        <td>{{$entry->num_instrument_app ?? ''}}</td>
+                        <td>{{$entry->day_landings ?? ''}}</td>
+                        <td>{{$entry->night_landings ?? ''}}</td>
+                        <td>{{$entry->total_duration ?? ''}}</td>
+                        <td class="text-truncate">{{!empty($entry->remarks) ? $entry->remarks : 'N/A'}}</td>
+                        <td>
+                            <div class="d-flex justify-content-between gap-2">
+                                <div>
+                                    <x:action route="entries.show" parameters="{{ $entry->getKey() }}">
+                                        <i class="fa-solid fa-eye"></i>
+                                    </x:action>
+                                </div>
+                                <div>
+                                    <x:action route="entries.edit" parameters="{{ $entry->getKey() }}">
+                                        <i class="fa-solid fa-pen-to-square"></i>
+                                    </x:action>
+                                </div>
+                                <div>
+                                    <form action="{{ route('entries.destroy', $entry->getKey()) }}" method="POST" id="form-delete-entries">
+                                        @csrf
+                                        @method('DELETE')
+                                        <x:action class="data-delete">
+                                            <i class="fa-solid fa-trash"></i>
+                                        </x:action>
+                                    </form>
+                                </div>
+                            </div>
+                        </td>
                     </tr>
-                    </thead>
-                    <tbody>
-                    @if(!empty($entries))
-                        @foreach($entries as $entry)
-                            <tr>
-                                <td class="text-truncate">{{$entry->date}}</td>
-                                <td>{{$entry->aircraft->identifier ?? ''}}</td>
-                                <td>{{$entry->category->category ?? ''}}</td>
-                                <td>{{$entry->category_time ?? ''}}</td>
-                                <td>{{$entry->type->type ?? ''}}</td>
-                                <td>{{$entry->type_time ?? ''}}</td>
-                                <td>{{$entry->day_time ?? ''}}</td>
-                                <td>{{$entry->night_time ?? ''}}</td>
-                                <td>{{$entry->xc_time ?? ''}}</td>
-                                <td>{{$entry->actual_instrument ?? ''}}</td>
-                                <td>{{$entry->sim_instrument ?? ''}}</td>
-                                <td>{{$entry->num_instrument_app ?? ''}}</td>
-                                <td>{{$entry->day_landings ?? ''}}</td>
-                                <td>{{$entry->night_landings ?? ''}}</td>
-                                <td>{{$entry->total_duration ?? ''}}</td>
-                                <td class="text-truncate">{{!empty($entry->remarks) ? $entry->remarks : 'N/A'}}</td>
-                                <td>
-                                    <div class="d-flex justify-content-between gap-2">
-                                        <div>
-                                            <x:action route="entries.show" parameters="{{ $entry->getKey() }}">
-                                                <i class="fa-solid fa-eye"></i>
-                                            </x:action>
-                                        </div>
-                                        <div>
-                                            <x:action route="entries.edit" parameters="{{ $entry->getKey() }}">
-                                                <i class="fa-solid fa-pen-to-square"></i>
-                                            </x:action>
-                                        </div>
-                                        <div>
-                                            <form action="{{ route('entries.destroy', $entry->getKey()) }}" method="POST" id="form-delete-entries">
-                                                @csrf
-                                                @method('DELETE')
-                                                <x:action class="data-delete">
-                                                    <i class="fa-solid fa-trash"></i>
-                                                </x:action>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                        @endforeach
-                    @endif
-                    </tbody>
-                </table>
-            </div>
-        </div>
+                @endforeach
+            @endif
+            </tbody>
+        </table>
+
     </div>
 
     <script>
