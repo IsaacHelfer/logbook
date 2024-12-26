983f473d86bd5b18055ad8e39966716aa61f37c3
Isaac <76602451+IsaacHelfer@users.noreply.github.com>
2024-12-26 08:55:31 -0500

    adding template for logbook settings

--- resources/views/logbook_settings/index.blade.php
+++ resources/views/logbook_settings/index.blade.php
@@ -2,4 +2,57 @@
 
 @section('content')
     <h1>Logbook Settings</h1>
+
+    <div class="row mt-3">
+        <!-- Types -->
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        Types
+                        <x-action>
+                            <i class="fa-solid fa-square-plus fa-xs"></i>
+                        </x-action>
+                    </h5>
+
+                    <div class="table-responsive">
+                        <table class="table table-hover table-striped mt-3">
+                            <thead>
+                            <tr>
+                                <th scope="col">Type</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Categories -->
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        Categories
+                        <x-action>
+                            <i class="fa-solid fa-square-plus fa-xs"></i>
+                        </x-action>
+                    </h5>
+
+                    <div class="table-responsive">
+                        <table class="table table-hover table-striped mt-3">
+                            <thead>
+                            <tr>
+                                <th scope="col">Category</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
