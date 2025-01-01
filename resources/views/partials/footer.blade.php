16854d23277a49b0a64b96b9ac7cce7bb530b49d
IsaacHelfer <ikelbereger30@gmail.com>
2025-01-01 03:14:14 -0500

    editing footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,13 @@
-<footer class="bg-light text-center py-3 mt-4">
-    <p>&copy; 2024 My Website</p>
-</footer>
+<div class="border-top mt-3">
+    <footer class="bg-light text-center py-3 ">
+        <p>
+            <span>
+                <i class="fa-regular fa-copyright"></i>
+            </span>
+            <span>
+                <strong>
+                    {{ date('Y') }} My Website
+                </strong>
+            </span>
+    </footer>
+</div>
