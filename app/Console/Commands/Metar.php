48e18f5a62d46f27ebb6daf4e1ec09ec3cd28715
Isaac <76602451+IsaacHelfer@users.noreply.github.com>
2025-01-10 14:35:49 -0500

    adding get metar command

---
+++ app/Console/Commands/Metar.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class Metar extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'get:metar {icao}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get METAR data.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $response = Http::get('https://aviationweather.gov/api/data/metar?ids=' . $this->argument('icao'));
+
+        if (!$response->successful()) {
+            $this->fail('Something went wrong!');
+        }
+
+        echo $response->body();
+    }
+}
