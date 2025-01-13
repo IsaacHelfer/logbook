37d473ddb680b02d842fd432011192761b041f9a
Isaac <76602451+IsaacHelfer@users.noreply.github.com>
2025-01-13 12:47:35 -0500

    updating todo list

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,11 +6,15 @@
     <img src="https://drive.google.com/thumbnail?id=1kGWG68thK4UYQci7x98JBENsagLn8rGf" alt="..." class="rounded float-left">
     <img src="https://drive.google.com/thumbnail?id=1nFhKx3hcJgkbGq12FMm_rpfUMUtQSG_W" alt="..." class="rounded float-left">
 
-    <h4>TODO</h4>
-    <ul class="list-group mt-4">
-        <li class="list-group-item">Do refactors on input component</li>
-        <li class="list-group-item">Implementation of entry tags</li>
-        <li class="list-group-item">Implementation of entry pictures</li>
-        <li class="list-group-item">Implementation of metar tool</li>
-    </ul>
+    {{--  Remove before LIVE  --}}
+    <div class="mt-3">
+        <h4>TODO</h4>
+        <ul class="list-group">
+            <li class="list-group-item">Refactors on input component</li>
+            <li class="list-group-item">Implementation of entry tags</li>
+            <li class="list-group-item">Implementation of entry pictures</li>
+            <li class="list-group-item">Implementation of metar tool</li>
+            <li class="list-group-item">Implementation of dark mode</li>
+        </ul>
+    </div>
 @endsection
