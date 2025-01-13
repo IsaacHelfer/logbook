8ecf741cfb14d69c1f7e1c01a98ae63cbc6a4cf9
Isaac <76602451+IsaacHelfer@users.noreply.github.com>
2025-01-13 14:48:28 -0500

    tried implementing metar

--- app/Livewire/Metar.php
+++ app/Livewire/Metar.php
@@ -2,12 +2,26 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Artisan;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Metar extends Component
 {
+    #[Validate('required')]
+    public $icao = '';
+
+    public $metar = '';
+
     public function render()
     {
         return view('livewire.metar');
+    }
+
+    public function getMetar()
+    {
+        $this->metar = Artisan::call('get:metar ' . $this->icao);
+
+        return $this->metar;
     }
 }
--- resources/views/livewire/metar.blade.php
+++ resources/views/livewire/metar.blade.php
@@ -1,16 +1,28 @@
 <div>
-    <form action="" method="GET">
+
+{{--        TODO: Add errors       --}}
+    <form wire:submit="getMetar">
         @csrf
 
         <div class="row">
             <div class="col-md-6 mb-3">
                 <label for="icao">ICAO</label>
-                <input type="text" id="icao" name="icao" class="form-control" />
+                <input type="text"
+                       id="icao"
+                       name="icao"
+                       class="form-control"
+                       wire:model="icao"
+                />
             </div>
             <div class="col-md-1 mb-3">
                 <label for=""></label>
-                <input type="submit" value="Get" class="btn btn-primary form-control col-md-6 mb-3" />
+                <input type="submit"
+                       value="Get"
+                       class="btn btn-primary form-control col-md-6 mb-3"
+                />
             </div>
         </div>
     </form>
+
+    <h1>{{ $metar }}</h1>
 </div>
