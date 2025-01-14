41478da7a114a23371d58dffaf7f49a684d69b3f
IsaacHelfer <ikelbereger30@gmail.com>
2025-01-13 20:10:23 -0500

    styling changes

--- resources/views/livewire/metar.blade.php
+++ resources/views/livewire/metar.blade.php
@@ -36,47 +36,53 @@
         @endphp
 
         <div class="row">
-            <div class="col-md-12 mb-4">
-                <i class="fa-solid fa-plane fa-2xl"></i>
-                <strong>{{ $data['icaoId'] . ', ' . $data['name'] }}</strong>
-            </div>
-            <div class="col-md-12 mb-4">
-                <i class="fa-solid fa-calendar-days fa-2xl"></i>
-                <strong>{{ $metar_time->format('M d, Y') }}</strong>
-            </div>
-            <div class="col-md-12 mb-4">
-                <i class="fa-solid fa-clock fa-2xl"></i>
-                <strong>{{ $metar_time->format('Hi\z') }}</strong>
-            </div>
-            <div class="col-md-12 mb-4">
-                <i class="fa-solid fa-wind fa-2xl"></i>
-                <strong>{{ $data['wdir'] . ' @ ' . $data['wspd'] }} {{ !is_null($data['wgst']) ? ' G ' . $data['wgst'] : '' }}</strong>
-            </div>
-            <div class="col-md-12 mb-4">
-                <i class="fas fa-eye fa-2xl"></i>
-                <strong>{{ $data['visib'] . ' sm' }}</strong>
-            </div>
-            <div class="col-md-12 mb-4">
-                <i class="fa-solid fa-cloud fa-2xl"></i>
-                @if(!empty($data['clouds']))
-                    @foreach($data['clouds'] as $cloud_data)
-                        <strong>{{ $cloud_data['cover'] }} {{ !is_null($cloud_data['base']) ? $cloud_data['base'] : '' }}</strong>
-                    @endforeach
-                @else
-                    <strong>CLR</strong>
-                @endif
-            </div>
-            <div class="col-md-12 mb-4">
-                <i class="fas fa-thermometer fa-2xl"></i>
-                <strong>{{ $data['temp'] }} &deg; C</strong>
-            </div>
-            <div class="col-md-12 mb-4">
-                <i class="fas fa-droplet fa-2xl"></i>
-                <strong>{{ $data['dewp'] }} &deg; C</strong>
-            </div>
-            <div class="col-md-12 mb-4">
-                <i class="fas fa-tachometer-alt fa-2xl"></i>
-                <strong>{{ number_format((int)$data['altim'] / 33.8639, 2) . ' inHg' }}</strong>
+            <div class="col-md-6 mb-4">
+                <div class="card shadow-sm">
+                    <div class="card-body">
+                        <div class="col-md-12 mb-4">
+                            <i class="fa-solid fa-plane fa-2xl"></i>
+                            <strong>{{ $data['icaoId'] . ', ' . $data['name'] }}</strong>
+                        </div>
+                        <div class="col-md-12 mb-4">
+                            <i class="fa-solid fa-calendar-days fa-2xl"></i>
+                            <strong>{{ $metar_time->format('M d, Y') }}</strong>
+                        </div>
+                        <div class="col-md-12 mb-4">
+                            <i class="fa-solid fa-clock fa-2xl"></i>
+                            <strong>{{ $metar_time->format('Hi\z') }}</strong>
+                        </div>
+                        <div class="col-md-12 mb-4">
+                            <i class="fa-solid fa-wind fa-2xl"></i>
+                            <strong>{{ $data['wdir'] . ' @ ' . $data['wspd'] }} {{ !is_null($data['wgst']) ? ' G ' . $data['wgst'] : '' }}</strong>
+                        </div>
+                        <div class="col-md-12 mb-4">
+                            <i class="fas fa-eye fa-2xl"></i>
+                            <strong>{{ $data['visib'] . ' sm' }}</strong>
+                        </div>
+                        <div class="col-md-12 mb-4">
+                            <i class="fa-solid fa-cloud fa-2xl"></i>
+                            @if(!empty($data['clouds']))
+                                @foreach($data['clouds'] as $cloud_data)
+                                    <strong>{{ $cloud_data['cover'] }} {{ !is_null($cloud_data['base']) ? $cloud_data['base'] : '' }}</strong>
+                                @endforeach
+                            @else
+                                <strong>CLR</strong>
+                            @endif
+                        </div>
+                        <div class="col-md-12 mb-4">
+                            <i class="fa-solid fa-temperature-full fa-2xl"></i>
+                            <strong>{{ $data['temp'] }} &deg; C</strong>
+                        </div>
+                        <div class="col-md-12 mb-4">
+                            <i class="fas fa-droplet fa-2xl"></i>
+                            <strong>{{ $data['dewp'] }} &deg; C</strong>
+                        </div>
+                        <div class="col-md-12">
+                            <i class="fas fa-tachometer-alt fa-2xl"></i>
+                            <strong>{{ number_format((int)$data['altim'] / 33.8639, 2) . ' inHg' }}</strong>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     @endforeach
